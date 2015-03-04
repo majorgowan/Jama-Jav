ab34980a3fb287a1977e7167a40acbd359fed270
Mark Fruman <mfruman@iau.uni-frankfurt.de>
2015-03-04 18:11:45 -0500

    fixed TrackPanel.mainPanel behaviour (still odd that second Track added so far down ... not serious

--- jamajav/TrackPanel.java
+++ jamajav/TrackPanel.java
@@ -16,11 +16,9 @@ import java.io.*;
 
 class TrackPanel extends JPanel implements ActionListener {
 
-    final private int DEFAULT_WIDTH = 500;
-    final private int DEFAULT_HEIGHT = 500;
-
     private ArrayList<Track> tracks;
     private ArrayList<JCheckBox> trackCheckBoxes;
+    private ArrayList<JPanel> linePanel;
     private int ntracks = 0;
 
     private JPanel mainPanel;
@@ -34,6 +32,7 @@ class TrackPanel extends JPanel implements ActionListener {
         if (comStr.equals("New Track")) {
             tracks.add(new Track(metronome, clock));
             trackCheckBoxes.add(new JCheckBox("",true));
+            linePanel.add(new JPanel());
             ntracks++;
 
             System.out.println("adding track ... now " + ntracks + " tracks");
@@ -41,25 +40,22 @@ class TrackPanel extends JPanel implements ActionListener {
             tracks.get(ntracks-1)
                 .setBorder(BorderFactory.createRaisedBevelBorder());
 
-            mainPanel.add(trackCheckBoxes.get(ntracks-1));
-            mainPanel.add(tracks.get(ntracks-1));
+            linePanel.get(ntracks-1).add(trackCheckBoxes.get(ntracks-1));
+            linePanel.get(ntracks-1).add(tracks.get(ntracks-1));
+            mainPanel.add(linePanel.get(ntracks-1));
             mainPanel.add(Box.createRigidArea(new Dimension(0,10)));
             revalidate();
         } else if (comStr.equals("Remove Selected")) {
             for (int i = tracks.size() - 1; i >= 0; i--)
                 if (trackCheckBoxes.get(i).isSelected()) {
-                    mainPanel.remove(trackCheckBoxes.get(i));
-                    mainPanel.remove(tracks.get(i));
+                    mainPanel.remove(linePanel.get(i));
                     tracks.remove(i);
                     trackCheckBoxes.remove(i);
+                    linePanel.remove(i);
                     ntracks--;
                 }
             repaint();
         }
-    }
-
-    public Dimension getPreferredSize() {
-        return (new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT));
     }
 
     TrackPanel(Metronome m, Clock c) {
@@ -69,13 +65,14 @@ class TrackPanel extends JPanel implements ActionListener {
 
         tracks = new ArrayList<Track>(0);
         trackCheckBoxes = new ArrayList<JCheckBox>(0);
+        linePanel = new ArrayList<JPanel>(0);
 
         setBackground(new Color(0.75f,0.6f,0.1f));
         setLayout(new BorderLayout());
 
         mainPanel = new JPanel();
-        //mainPanel.setLayout(new BoxLayout(mainPanel,BoxLayout.PAGE_AXIS));
-        mainPanel.setLayout(new FlowLayout());
+        mainPanel.setLayout(new BoxLayout(mainPanel,BoxLayout.PAGE_AXIS));
+        //mainPanel.setLayout(new FlowLayout());
         
         JPanel buttonPanel = new JPanel(new FlowLayout());
 
@@ -95,7 +92,9 @@ class TrackPanel extends JPanel implements ActionListener {
         mainPanel.setBorder(BorderFactory.createRaisedBevelBorder());
         buttonPanel.setBorder(BorderFactory.createRaisedBevelBorder());
 
-        add(mainPanel,BorderLayout.CENTER);
+        MainScrollPane scrollPane = new MainScrollPane(mainPanel);
+
+        add(scrollPane,BorderLayout.CENTER);
         add(buttonPanel,BorderLayout.SOUTH);
     }
 }
