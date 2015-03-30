af9cb359d3b3c5b3bce89f63a9f804bb0061dff8
Mark Fruman <fruman@iau.uni-frankfurt.de>
2015-03-29 23:57:55 -0400

    auto-select TitleField in Info editor

--- jamajav/InfoPanel.java
+++ jamajav/InfoPanel.java
@@ -124,6 +124,7 @@ class InfoPanel extends JPanel implements ActionListener {
         add(buttonPanel, BorderLayout.SOUTH);
 
         titleField.requestFocusInWindow();
+        titleField.selectAll();
     }
 
 }
